c16b82f3ab95ea6515f41191fb1b686526453526
Allibaba112 <allibaba112@gmail.com>
2024-12-30 12:55:33 -0500

    react router test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing_page from './components/Landing_Page/Landing_page'};
+import Navbar from './Components/Navbar/Navbar';
+import ReactDOM from "react-dom/client";
 import logo from './logo.svg';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* Set up BrowserRouter for routing */}
+        <BrowserRouter>
+          {/* Display the Navbar component */}
+          <Navbar/>
+          {/* Set up the Routes for different pages */}
+          <Routes>
+            {/* Define individual Route components for different pages */}
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
